f9aba2b9e4f428cff936b9680f513537bb2cacf0
Eugene Kim <eugenek0529@gmail.com>
2023-10-19 13:36:40 -0700

    classes folders created

---
+++ classes/Account.java
@@ -0,0 +1,42 @@
+public class Account { 
+
+    // Attributes
+    private int userID;
+    private int phoneNumber;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+
+    // Login Credentials
+    private String loginID;
+    private String loginPassword;
+    private boolean isLoggedIn; 
+
+
+    // constructor
+    public Account()
+    {
+
+
+    }
+
+    /*  some of the methods to work on 
+
+        - login functino
+            - how to change isLoggedIn to true and false
+
+        - How to add a new Address 
+            - validation
+            - set one of them to isPriority true, 
+            - when one become true, change others to false
+
+        - How to add a new credit card info
+            - validation
+            - set one of them to isPriority true
+            - when one become true, change others to false
+            
+
+    */
+
+
+}
