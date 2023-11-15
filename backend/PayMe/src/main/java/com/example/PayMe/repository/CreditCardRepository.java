d5b7b9f2913b6d241b6028c924abd33f64d958fa
Eugene Kim <eugenek0529@gmail.com>
2023-11-14 22:03:28 -0800

    .

--- backend/PayMe/src/main/java/com/example/PayMe/repository/CreditCardRepository.java
+++ backend/PayMe/src/main/java/com/example/PayMe/repository/CreditCardRepository.java
@@ -1,6 +1,7 @@
 package com.example.PayMe.repository;
 
+import com.example.PayMe.entity.CreditCardInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CreditCardRepository extends JpaRepository<> {
+public interface CreditCardRepository extends JpaRepository<CreditCardInfo,String> {
 }
