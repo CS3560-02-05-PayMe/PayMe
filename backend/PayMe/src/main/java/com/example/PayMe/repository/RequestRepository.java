254eb60284913ce0b5da69d09013886387dfc867
danielm925 <89175100+danielm925@users.noreply.github.com>
2023-11-16 21:59:49 -0800

    Update RequestRepository.java

--- backend/PayMe/src/main/java/com/example/PayMe/repository/RequestRepository.java
+++ backend/PayMe/src/main/java/com/example/PayMe/repository/RequestRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface RequestRepository extends JpaRepository<Account, UUID> {
+public interface RequestRepository extends JpaRepository<Request, UUID> {
 }
