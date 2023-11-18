d37ec5f740fe47756359c5e10f540b2da469c8cc
Ethan <etarrer@gmail.com>
2023-11-18 00:12:25 -0800

    Edited and added functionality to the backend with TransactionService.java and TransactionController.java

--- backend/src/main/java/com/example/PayMe/service/TransactionService.java
+++ backend/src/main/java/com/example/PayMe/service/TransactionService.java
@@ -1,24 +1,33 @@
-//package com.example.PayMe.service;
-//
-//import com.example.PayMe.entity.Transaction;
-//import com.example.PayMe.repository.TransactionRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class TransactionService {
-//    @Autowired
-//    private TransactionRepository repository;
-//
-//
-//    // Write get, post, delete, update methods
-//
-//    //-----------------------------------------
-//    // Post methods
-//
-//    public Transaction saveTransaction(Transaction transaction)
-//    {
-////        return transaction.save(transaction);
-//        return null;
-//    }
-//}
+package com.example.PayMe.service;
+
+import com.example.PayMe.entity.Transaction;
+import com.example.PayMe.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TransactionService {
+    
+    @Autowired
+    private TransactionRepository repository;
+
+    // This method retrieves all transactions from the database and sorts them in descending order by date.
+    public List<Transaction> getAllTransactions() {
+        return repository.findAll(Sort.by(Sort.Direction.DESC, "date"));
+    }
+
+    // This method creates a new transaction in the database.
+    public Transaction createTransaction(Transaction transaction) {
+        return repository.save(transaction);
+    }
+
+    // This method retrieves a transaction from the database by its ID.
+    public Optional<Transaction> getTransactionById(UUID transactionId) {
+        return repository.findById(transactionId);
+    }
+}
