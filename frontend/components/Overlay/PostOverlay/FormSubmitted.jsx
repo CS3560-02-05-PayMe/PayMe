7b6b44ead1c913133c3afd7396aa45a7e6c0302c
Simon Ly <50601011+SiLyGoose@users.noreply.github.com>
2023-11-22 17:48:52 -0800

    Pay/Request UI change

--- frontend/components/Overlay/PostOverlay/FormSubmitted.jsx
+++ frontend/components/Overlay/PostOverlay/FormSubmitted.jsx
@@ -36,9 +36,9 @@ export default function FormSubmitted({ message, imageSrc, onRelease }) {
 		<div className={clsx("d-flex flex-column w-100 h-100", styles.formSubmitted)}>
 			<div className="d-flex w-100 h-100 justify-content-center">{/* <img src={imageSrc} className={clsx("w-100", styles.fadeIn)} style={{ maxWidth: "250px" }} /> */}</div>
 			<div className="d-flex flex-column w-100 mt-2 justify-content-center align-items-center">
-				<svg id="checkmark" class={clsx("checkmark d-flex text-center m-auto", styles.formCheckmark)} xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 32 32">
-					<circle class="circle" cx="16" cy="16" r="16" fill="#00b32d" />
-					<path class="check" d="M9 16l5 5 9-9" fill="none" stroke="#fff" stroke-width="2.5" stroke-linecap="round" />
+				<svg id="checkmark" className={clsx("checkmark d-flex text-center m-auto", styles.formCheckmark)} xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 32 32">
+					<circle className="circle" cx="16" cy="16" r="16" fill="#00b32d" />
+					<path className="check" d="M9 16l5 5 9-9" fill="none" stroke="#fff" stroke-width="2.5" stroke-linecap="round" />
 				</svg>
 				<div className="my-4">
 					<span className={clsx(typingStyles.fontType9)}>{message}</span>
--- frontend/components/Overlay/RequestForm.jsx
+++ frontend/components/Overlay/RequestForm.jsx
@@ -17,7 +17,9 @@ export default function RequestForm({ apply, onRelease }) {
 	const [amount, setAmount] = useState(0);
 	const [message, setMessage] = useState("");
 
-	const formInputs = [
+	const [step, setStep] = useState(1);
+
+	const firstFormInput = [
 		<input
 			type="text"
 			placeholder="Request From"
@@ -28,6 +30,9 @@ export default function RequestForm({ apply, onRelease }) {
 				setDebtor(formattedPayer);
 			}}
 		/>,
+	];
+
+	const secondFormInput = [
 		<input
 			type="text"
 			placeholder="Amount"
@@ -48,6 +53,11 @@ export default function RequestForm({ apply, onRelease }) {
 		/>,
 	];
 
+	const toggleStep = (event) => {
+		event.preventDefault();
+		setStep((step === 1) + 1);
+	};
+
 	// send request to mentioned account's inbox
 	const handleSubmit = async (event) => {
 		event.preventDefault();
@@ -56,10 +66,32 @@ export default function RequestForm({ apply, onRelease }) {
 	};
 
 	return (
-		<Form formType={"Request"} formAltType={"Request Sent"} formInputs={formInputs} onSubmit={handleSubmit} onRelease={onRelease} formAltSrc={"requestSent"}>
-			<button type="submit" className={clsx(styles.formSubmit, styles.loginButton)}>
-				Request
-			</button>
+		<Form formType={"Request"} formAltType={"Request Sent"} onSubmit={handleSubmit} onRelease={onRelease} formAltSrc={"requestSent"}>
+			<fieldset className={clsx("w-100", { [styles.hide]: step > 1 })}>
+				{firstFormInput.map((item, index) => (
+					<div key={index} className={clsx("my-3", styles.formInput)}>
+						{item}
+					</div>
+				))}
+				<button className={clsx(styles.formSubmit, styles.loginButton)} onClick={toggleStep}>
+					Next
+				</button>
+			</fieldset>
+			<fieldset className={clsx("w-100", { [styles.show]: step === 2 })}>
+				{secondFormInput.map((item, index) => (
+					<div key={index} className={clsx("my-3", styles.formInput)}>
+						{item}
+					</div>
+				))}
+				<div className={clsx("d-flex w-100 justify-content-between")}>
+					<button className={clsx(styles.formSubmit, styles.loginButton)} onClick={toggleStep}>
+						Previous
+					</button>
+					<button type="submit" className={clsx(styles.formSubmit, styles.loginButton)}>
+						Request
+					</button>
+				</div>
+			</fieldset>
 		</Form>
 	);
 }
